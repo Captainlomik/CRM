47bcded5880234da197cdbc49554f980072b73a7
DeadInsideKoala <nastiy.348.0212@gmail.com>
2020-11-26 15:14:38 +0300

    add controllers. finish download back

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    mongoURI:'mongodb+srv://admin:O194hPygRIqMkPQC@cluster0.xt4pm.mongodb.net/MEAN?retryWrites=true&w=majority',
+    jwt:'dev-jwt'
+}
---
+++ controllers/order.js
@@ -0,0 +1,58 @@
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
+//(get) localhost:4200/order?offset=2&limit=5
+module.exports.getAll = async function (req, res) {
+    const query = {
+        user:req.user.id
+    }
+    //Дата старта - фильтрация по дате
+    if (req.query.start){
+        query.date={
+            $gte:req.query.start //<=
+        }
+    }
+    if(req.query.end){
+        if(!query.date){
+            query.date={}
+        }
+        query.date['$lte'] = req.query.end
+    }
+    if(req.query.order){ //поиск 
+        query.order=+req.qury.order
+    }
+
+
+    try {
+        const order=await Order
+        .find(query)
+        .sort({date:-1})
+        .skip(+req.query.offset)
+        .limit(+req.query.limit)
+
+        res.status(200).json(orders)
+
+    } catch (err) {
+        errorHandler(res, err)
+    }
+}
+
+module.exports.create = async function (req, res) {
+    try {
+         const lastOrder=await Order
+         .findOne({ user:req.user.id})
+         .sort({date:-1})
+
+         const maxOrder=lastOrder? lastOrder.order : 0
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order:maxOrder+1,
+
+        }).save()
+        res.status(201).json(order)
+    } catch (err) {
+        errorHandler(res, err)
+    }
+}
