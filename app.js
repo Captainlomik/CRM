8b0fec1b74837aea3cb9326503577a6968c0bc7e
CaptainLomik <nastiy248@mail.ru>
2020-12-03 21:43:35 +0300

    initial

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const passport=require('passport')
+const passport = require('passport')
+const path = require('path')
 
 const authRoutes = require('./routes/auth')
 const analiticsRoutes = require('./routes/analytics')
@@ -12,14 +13,18 @@ const keys = require('./config/keys')
 
 const app = express()
 
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
-    useNewUrlParser: true})
+mongoose.connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true
+    })
     .then(() => console.log('MongoBD connected'))
-        .catch(err => console.log(err))
+    .catch(err => console.log(err))
 
 app.use(passport.initialize())
-require('./middleweare/passport')(passport)        
-        
+require('./middleweare/passport')(passport)
+
 app.use(require('morgan')('dev'))
 app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({
@@ -28,10 +33,23 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use(require('cors')())
 
-app.use('/api/auth',  authRoutes)
+app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analiticsRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist/client'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        )
+    })
+}
+
 
 module.exports = app;
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-module.exports={
-    mongoURI:'mongodb+srv://admin:O194hPygRIqMkPQC@cluster0.xt4pm.mongodb.net/MEAN?retryWrites=true&w=majority',
-    jwt:'dev-jwt'
+if(process.env.NODE_ENV==='production')
+{
+    module.exports=require('./keys.prod')
+}
+else {
+    module.exports=require('./keys.dev')
 }
